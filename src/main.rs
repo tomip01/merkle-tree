95f8e532a10cb87f850b5ee3326a14f39efda2f0
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2024-12-20 13:25:49 -0300

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ use merkle::MerkleTree;
 mod merkle;
 
 fn main() {
+    // create a tree
     let data: Vec<&[u8]> = vec![b"this", b"is", b"a", b"merkle", b"tree"];
     let mut merkle = MerkleTree::new(&data);
+
+    // generate a proof
     let proof = merkle.generate_proof(b"is").unwrap();
     let hashes = &proof.hashes;
     let root = &proof.root;
     let index = proof.index;
-    let _verified = merkle.verify(hashes, &hash(b"is"), root, index);
-    merkle.add(b"last");
 
-    println!("hello world!");
+    // verify is a valid proof
+    let verified = merkle.verify(hashes, &hash(b"is"), root, index);
+    println!("Can the proof be verified? {verified}");
+
+    // add element to the tree
+    merkle.add(b"last");
 }
